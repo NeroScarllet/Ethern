1dcd0f48355cddd0287760d6fd0d476667362b13
NeroScarllet <kaykyr06@gmail.com>
2024-10-17 17:42:57 -0300

    First Page

---
+++ android/app/src/main/kotlin/com/example/ethern/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ethern
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:ethern/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'MedievalSharp',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
